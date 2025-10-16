d8c38020bded44df65098ad8f237469e62dffd94
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-10-16 18:45:32 +0300

    update

--- src/services/AccessGeolocationAPI.js
+++ src/services/AccessGeolocationAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const AccessGeolocationAPI = () => {
   return axios.get(
-    "https://ip-api.com/json/?fields=status,message,country,city,lat,lon"
+    "http://ip-api.com/json/?fields=status,message,country,city,lat,lon"
   );
 };
 export default AccessGeolocationAPI;
